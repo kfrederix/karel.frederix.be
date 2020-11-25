a4033e319e8a271e193d6d8ff65b62c57c10ce3e
Karel Frederix <karel.frederix@selligent.com>
2020-11-25 22:43:45 +0100

    add favicon.ico and fix icon

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -20,9 +20,13 @@
 </style>
 
 <svelte:head>
-  <link rel="stylesheet" href="/style.css" />
   <title>Karel Frederix</title>
   <meta name="description" content="Karel Frederix is a Software Engineer based in Hasselt, BE" />
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <link rel="stylesheet" href="/style.css" />
 </svelte:head>
 <Header aboutMe={data.aboutMe} permalink={request.permalink} />
 <section>
