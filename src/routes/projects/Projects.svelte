4d2bffe2e7edc55ff54aace55ddc9b15f020da10
Karel Frederix <karel.frederix@gmail.com>
2020-11-29 13:22:52 +0100

    tweak project photos styling

--- src/routes/projects/Projects.svelte
+++ src/routes/projects/Projects.svelte
@@ -26,7 +26,8 @@
     width: 100%;
     height: auto;
     border-radius: 0.5rem;
-    box-shadow: 0 10px 15px -3px rgba(0,0,0,.1), 0 4px 6px -2px rgba(0,0,0,.05);
+    /*box-shadow: 0 10px 15px -3px rgba(0,0,0,.1), 0 4px 6px -2px rgba(0,0,0,.05);*/
+    border: solid 1px #efefef;
   }
   h2 {
     margin-top: 0;
