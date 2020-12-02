bf6074764d0850a0f9835ac4ab0a085edd4d729b
Karel Frederix <karel.frederix@selligent.com>
2020-12-02 11:05:20 +0100

    navar fix on top

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,6 +8,11 @@
     padding: 0;
     margin: 0;
     background-color: var(--header-bg-color);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 30;
   }
   .header-container {
     display: flex;
