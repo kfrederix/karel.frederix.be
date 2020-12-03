a92a4513a15c12da48ddc318c031e17e1ee3251d
Karel Frederix <karel.frederix@selligent.com>
2020-12-03 09:30:37 +0100

    small hero improvements

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,5 @@
 <script>
   export let avatarUrl;
-  export let title;
-  export let subTitle;
 </script>
 
 <style>
@@ -9,11 +7,21 @@
     color: #eee;
     margin: 1.5rem 0 1rem 0;
     font-size: 2.5rem;
+    font-weight: 600;
   }
   h2 {
-    margin: 0.5rem 0 0 0;
-    font-size: 1.3rem;
-    color: #cdd;
+    margin: 2rem 0 0 0;
+    font-size: 1.7rem;
+    font-weight: 700;
+    color: #bbb;
+    line-height: 1.5;
+  }
+  h3 {
+    margin: 0;
+    font-size: 1.25rem;
+    font-weight: 500;
+    line-height: 1.5;
+    color: #6aa;
   }
   .hero {
     background-color: var(--header-bg-color);
@@ -29,9 +37,6 @@
     max-width: 940px;
     margin: 0 auto;
     box-sizing: border-box;
-  }
-  .hero-inner-part {
-    /*padding: 0 1rem;*/
   }
   .avatar {
     vertical-align: middle;
@@ -55,17 +60,18 @@
 
 <div class="hero">
   <div class="hero-inner">
-    <div class="hero-inner-part">
+    <div>
       <!--
         Best Practice:
-          specifying width and height on the img tag explicitly allows 
-          the image to take up the space before it loads
+         specifying width and height on the img tag explicitly allows 
+         the image to take up the space before it loads
       -->
       <img class="avatar" alt="avatar" src="{avatarUrl}?h=361" width="120" height="120"/>
     </div>
-    <div class="hero-inner-part">
-    <h1>{title}</h1>
-    <h2>{subTitle}</h2>
+    <div>
+      <h1>Hi! I'm Karel.</h1>
+      <h2>I write code.</h2>
+      <h3>Mostly for web.</h3>
     </div>
   </div>
 </div>
