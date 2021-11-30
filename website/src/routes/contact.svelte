1cd932e3d7af9d6e06df734160f49e272d8ec3a8
Karel Frederix <kfrederix@selligent.com>
2021-11-30 11:41:59 +0100

    update linkedin icon

--- website/src/routes/contact.svelte
+++ website/src/routes/contact.svelte
@@ -20,8 +20,8 @@
     </div>
     <div class="flex items-center">
       <span class="mr-3">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-0.5" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-0.5" aria-hidden="true" focusable="false" role="img" viewBox="0 0 448 512">
+          <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
         </svg>
       </span>
       <a target="_blank" rel="external noopener" href="https://linkedin.com/in/kfrederix">linkedin.com/in/kfrederix</a>
